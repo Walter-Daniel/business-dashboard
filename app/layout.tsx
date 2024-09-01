49e147cbd6557462951e07a741f6275ef89821e3
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-09-01 07:21:28 -0300

    add: Create Sign In and Sign Up route, create auth layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_Display } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import {ClerkProvider} from '@clerk/nextjs'
 import "./globals.css";
 
 const noto = Noto_Sans_Display({ subsets: ["latin"] });
@@ -25,12 +19,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={noto.className}>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>
