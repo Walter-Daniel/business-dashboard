83bd96732367851506b7b1137ce3f79caae75e9a
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-09-11 11:00:21 -0300

    fix: event and company api routes

--- app/api/company/[companyId]/contact/route.ts
+++ app/api/company/[companyId]/contact/route.ts
@@ -2,43 +2,35 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-interface Props {
-    params: {
-        companyId: string
+export async function POST(req: Request, { params }: { params: { companyId: string } }) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
-}
 
-export async function POST(req:Request, {params}:Props){
+    const { companyId } = params;
+    const company = await db.company.findFirst({
+      where: {
+        id: companyId,
+      },
+    });
 
-    try {
-        const { userId } = auth();
-        if(!userId){
-            return new NextResponse("Unauthorize", {status:401})
-        };
-
-        const { companyId } = params;
-        const company = await db.company.findFirst({
-            where:{
-                id: companyId,
-            }
-        })
-
-        if(!company){
-            return new NextResponse("Company Not Found.", {status:401})
-        }
-
-        const data = await req.json()
-
-        const contact = await db.contact.create({
-            data: {
-                companyId,
-                ...data
-            }
-        });
-
-        return NextResponse.json(contact);
-    } catch (error) {
-        console.log("[CONTACT]", error);
-        return new NextResponse("Internal Error", {status:500})
+    if (!company) {
+      return new NextResponse("Company Not Found.", { status: 404 });
     }
+
+    const data = await req.json();
+
+    const contact = await db.contact.create({
+      data: {
+        companyId,
+        ...data,
+      },
+    });
+
+    return NextResponse.json(contact);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
